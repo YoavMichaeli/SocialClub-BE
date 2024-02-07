86c6591e9d7a488a8776f67e4d27ed6fa3200af6
omerTiger <omertiger6@gmail.com>
2024-02-07 19:36:12 +0200

    Adding user profile route

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -38,3 +38,67 @@ router.get("/myposts", function (req, res, next) {
     });
   });
 });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, '../public/profile_pics/')); // Specify the destination directory
+  },
+  filename: (req, file, cb) => {
+    const finalFileName = guid.raw() + '_' + file.originalname;
+    cb(null, finalFileName); // Specify the filename
+  },
+});
+
+const upload = multer({ storage: storage });
+
+
+router.patch("/profile", upload.single('file') ,async function (req, res, next) {
+  const newFirstName = req.body.firstname;
+  const newLastName = req.body.lastname;
+
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+  const payload = jwt.decode(token, { complete: true }).payload;
+  let user = await db.asyncFindById({ _id: payload._id });
+
+  if(req.file) {
+    const filePath = path.join(__dirname, `../public${user.profile_pic}`)
+    fs.unlink(filePath, (err) => {
+      if (err) {
+        console.error('Error deleting file:', err);
+      } else {
+        console.log('File deleted successfully');
+      }
+    });
+    console.log(req.file);
+    const uploadedFile = req.file;
+    let final_location = `/profile_pics/${uploadedFile.filename}`;
+    user.profile_pic = final_location;
+  }
+  
+  if (newFirstName) {
+    user.firstname = newFirstName;
+  }
+
+  if (newLastName) {
+    user.lastname = newLastName;
+  }
+
+  user.save((err, result) => {
+    if (err) throw err;
+    res.status(200).send("Profile updated");
+  });
+});
+
+router.get("/me", function (req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1]; // Bearer <token>
+  const payload = jwt.decode(token, { complete: true }).payload;
+  db.findOne({ username: payload.user }, (err, user) => {
+    res.status(200).json({
+      user: user,
+    });
+  });
+});
+
+module.exports = router;
