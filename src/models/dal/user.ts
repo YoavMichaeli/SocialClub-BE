5040df3279bf500a96aa0b1338c8230e38f31fc9
omerTiger <omertiger6@gmail.com>
2024-02-07 19:41:41 +0200

    Adding check user logic and export all the methods

--- src/models/dal/user.ts
+++ src/models/dal/user.ts
@@ -69,3 +69,142 @@ function editUser(obj, cb) {
     });
   }
 }
+
+function checkUser(obj, cb) {
+  User.findOne({ username: obj.username }).exec((err, user) => {
+    console.log(user);
+    if (err) return cb(err, false);
+    if (user) {
+      bcrypt.compare(obj.password, user.password, (err, bool) => {
+        if (bool) {
+          return cb(null, user);
+        } else {
+          return cb(null, false);
+        }
+      });
+    } else {
+      return cb(null, false);
+    }
+  });
+}
+
+function findOne(obj, cb) {
+  User.findOne(obj).exec((err, user) => {
+    if (err) return cb(err, false);
+    if (user) {
+      return cb(err, user);
+    } else {
+      return cb(null, false);
+    }
+  });
+}
+
+
+function findById(id, cb) {
+  User.findById(id).exec((err, user) => {
+    if (err) return cb(err, false);
+    if (user) {
+      return cb(err, user);
+    } else {
+      return cb(null, false);
+    }
+  });
+}
+
+function asyncFindById(id) {
+  return new Promise((resolve, reject) => {
+    User.findById(id).exec((err, user) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(user)  
+      }
+    });
+  });
+  
+}
+
+function search(opt, cb) {
+  User.find({ username: { $gt: opt } }).exec((err, results) => {
+    if (err) return cb(err, false);
+    if (results) {
+      return cb(err, results);
+    } else {
+      return cb(null, false);
+    }
+  });
+}
+
+function getAll(cb) {
+  User.find({}).exec((err, users) => {
+    if (err) return cb(err, false);
+    if (users) {
+      return cb(null, users);
+    } else {
+      return cb(null, false);
+    }
+  });
+}
+
+function deleteOne(opt, cb) {
+  User.deleteOne(opt).exec((err, res) => {
+    if (err) return cb(err, null);
+    else if (res.n == 0) {
+      return cb(null, true);
+    }
+  });
+}
+function comment(user, comment, _id, cb) {
+  User.findOne(user).exec((err, obj) => {
+    if (!obj) return cb("Does not exist.", null);
+    console.log(obj);
+    for (var i = 0; i < obj.posts.length; i++) {
+      if (obj.posts[i]._id == _id) {
+        obj.posts[i].comments.push(comment);
+        obj.notifications.push({
+          msg: `@${comment.by} reacted to your post.`,
+          link: `/u/${comment.by}`,
+          time: new Date()
+        });
+        obj = new User(obj);
+        obj.save((err, res) => {
+          return cb(err, res);
+        });
+      }
+    }
+  });
+}
+function like(user, like, _id, cb) {
+  console.log(user);
+  User.findOne(user).exec((err, obj) => {
+    if (!obj) return cb("Does not exist.", null);
+    for (var i = 0; i < obj.posts.length; i++) {
+      if (obj.posts[i]._id == _id) {
+        obj.posts[i].likes.push(like.by);
+        obj.notifications.push({
+          msg: `@${like.by} liked your post.`,
+          link: `/u/${like.by}`,
+          time: new Date()
+        });
+        obj = new User(obj);
+        obj.save(err => {
+          cb(err, true);
+        });
+      }
+    }
+  });
+}
+
+module.exports = {
+  createNew: createNew,
+  editUser: editUser,
+  checkUser: checkUser,
+  findOne: findOne,
+  findById: findById,
+  deleteOne: deleteOne,
+  asyncFindById: asyncFindById,
+  getAll: getAll,
+  comment: comment,
+  like: like,
+  search: search
+};
