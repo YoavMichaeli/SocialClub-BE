84eb7b5d7fda72973937a4f4ed630873702b4d9e
Yoav <yoav3391@gmail.com>
2024-02-07 21:24:05 +0200

    Improving auth test cases

--- src/tests/api/auth.test.ts
+++ src/tests/api/auth.test.ts
@@ -2,26 +2,38 @@ const request = require("supertest");
 const app = require("../../app.ts");
 const jwt = require("jsonwebtoken");
 var db = require("../../models/dal/user.ts");
-const { postId } = require("../mocks/settings.ts");
 var mongoose = require("mongoose");
+const { testUserId, testUserName, testPassword } = require("../mocks/users.ts");
 var path = require('path');
+const fs = require('fs')
 
 let refreshToken, accessToken;
 
 beforeAll(async () => {
-  accessToken = jwt.sign({ _id: "65be6dfc8e04c4599cff3880", user: "test" }, process.env.JWT_SECRET, {
+  accessToken = jwt.sign({ _id: testUserId, user: testUserName }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRATION,
   });
-  refreshToken = jwt.sign({ _id: "65be6dfc8e04c4599cff3880", user: "test" }, process.env.JWT_REFRESH_SECRET, {
+  refreshToken = jwt.sign({ _id: testUserId, user: testUserName }, process.env.JWT_REFRESH_SECRET, {
     expiresIn: process.env.JWT_EXPIRATION,
   });
 
-  const user = await db.asyncFindById("65be6dfc8e04c4599cff3880");
+  const user = await db.asyncFindById(testUserId);
   user.refreshTokens = [refreshToken];
   await user.save();
 });
 
 afterAll(async () => {
+  const user = await db.asyncFindOne({username: 'test2'});
+  console.log(user);
+  await fs.unlink(path.join(__dirname, `../../public${user.profile_pic}`), (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log('Profile Pic deleted successfully');
+  });
+  await db.deleteOne({username: 'test2'});
+  await db.deleteOne({username: 'Yoav'});
   await mongoose.connection.close();
 });
 
@@ -34,37 +46,38 @@ describe("Account route tests (/account)", () => {
   });
 
 
-  test("/account/getin", async () => {
-    const response = await request(app).post("/account/getin").send({
-      username: "test",
-      password: "test",
-    });
-    expect(response.body).toHaveProperty("accessToken");
-    expect(response.body).toHaveProperty("refreshToken");
-    expect(response.statusCode).toBe(200);
-  });
-
   test("/account/new", async () => {
     const response = await request(app).post("/account/new")
     .field('username', "test2")
     .field('password', "test2")
     .field('firstname', "test2")
     .field('lastname', "test2")
-    .attach('file', path.join(__dirname, '../../public/profile_pics/d28a8ea6-d321-87bd-fa18-f955b015fa87_books-8405721_1280.jpg'));
+    .attach('file', path.join(__dirname, '../../public/feeds/test/5e08a20c-5c37-f6b1-442f-b9377753dbc1_books-8405721_1280.jpg'));
     expect(response.statusCode).toBe(201);
     });
 
 
-    test("/account/newGoogleUser", async () => {
+  test("/account/getin", async () => {
+    const response = await request(app).post("/account/getin").send({
+      username: testUserName,
+      password: testPassword,
+    });
+    expect(response.body).toHaveProperty("accessToken");
+    expect(response.body).toHaveProperty("refreshToken");
+    expect(response.statusCode).toBe(200);
+  });
+
+
+  test("/account/newGoogleUser", async () => {
         const response = await request(app).post("/account/newGoogleUser").send({
             token: process.env.TEST_GOOGLE_ACCESS_TOKEN
         })
         expect(response.statusCode).toBe(200);
-        });
+   });
 
 
   test("/account/out", async () => {
-    const user = await db.asyncFindById("65be6dfc8e04c4599cff3880");
+    const user = await db.asyncFindById(testUserId);
     const newRefreshToken = user.refreshTokens.pop();
     const response = await request(app).get("/account/out").set("Authorization", `Bearer ${newRefreshToken}`);
     expect(response.statusCode).toBe(200);
